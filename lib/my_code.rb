793ee20ac829bf62ed05bcff31377b334aaea535
Corey Hodge <contact@coreyhodge.net>
2019-12-31 05:16:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,12 +3,12 @@ map do
   i = 0
   freshArray = []
   while i < source_array.length do
-    freshArray [i] = source_array[i] yield 
+    freshArray [i] = source_array[i] yield
     i += 1
   end
   freshArray
 end
 
 reduce do
-  
+
 end
